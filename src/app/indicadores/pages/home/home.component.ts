91b05d71aa3e06925887c80ad4299ec88be72350
YonnathanDev <yonnathan.placencia.dev>
2022-09-08 19:48:48 -0300

    Se creó el menú y se añadieron sus opciones

--- src/app/indicadores/pages/home/home.component.ts
+++ src/app/indicadores/pages/home/home.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   styles: [`
-
+    .container {
+      margin-top: 50px
+    }
 `]
 })
 export class HomeComponent implements OnInit {
-
 
   constructor() { }
 
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,27 +2,29 @@ import { NgModule } from '@angular/core';
 
 import { MatSliderModule } from '@angular/material/slider';
 import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
 
+import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatTableModule} from '@angular/material/table';
 import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   exports: [
     MatSliderModule,
     MatButtonModule,
+    
     MatSidenavModule,
-
     MatGridListModule,
     MatToolbarModule,
     MatIconModule,
     MatDividerModule,
     MatTableModule,
-    MatCardModule
+    MatCardModule,
+    MatListModule
   ]
 })
 export class MaterialModule { }
