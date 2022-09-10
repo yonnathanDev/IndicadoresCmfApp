3e5d1869ad9cc5bcd456c630892eed2365e5a955
YonnathanDev <yonnathan.placencia.dev>
2022-09-10 03:07:38 -0300

    Indicadores/gáficos: Se actualizaron he incorporaron los datos del servicio indicadores.service actualizado

--- src/app/indicadores/pages/grafico/grafico.component.ts
+++ src/app/indicadores/pages/grafico/grafico.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Chart, ChartConfiguration, ChartEvent, ChartType } from 'chart.js';
+import { Chart, ChartConfiguration, ChartEvent, ChartData ,ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
+import { Indicador, Dolare } from '../../interfaces/indicadores';
 
-// import DataLabelsPlugin from 'chartjs-plugin-datalabels';
+import { IndicadorService } from '../../services/indicador.service';
+import { option } from '../detalle/detalle.component';
+
+import * as moment from 'moment/moment';
+
 
 @Component({
   selector: 'app-grafico',
@@ -12,34 +17,67 @@ import { BaseChartDirective } from 'ng2-charts';
 })
 export class GraficoComponent implements OnInit {
 
+  header: number[] = [];
+  labels: string[] = [];
+  indicador!: Indicador[];
   
+  op: option = {
+    name: 'ipc' ,
+    category: 3,
+    type: 'IPCs',
+    year: '',
+    month: '',
+    day: ''
+  }  
 
-  constructor() { }
+   // Line
+  public lineChartLabels: string[] = [ ];
+  
+  public lineChartData: ChartData<'line'> = {
+    labels: this.lineChartLabels,
+    datasets: []
+  };
+
+  public lineChartType: ChartType = 'line';    
+  
+  constructor( private indicadorService: IndicadorService ) { }
 
   ngOnInit(): void {
 
+    this.getDate(this.op.category);
+    console.log( this.op )
+
+    this.indicadorService.getData( this.op )
+        .subscribe( ({data}) => {
+          let objeto = data.IPCs;
+          
+          for( let i in  objeto){
+            let header =  parseFloat(objeto[i].Valor);
+            this.header.push( header );
+            this.labels.push( objeto[i].Fecha );
+          }
+
+          const colors = ['#6405F0','#0724E3', '#05A0F0','#0724E3', '#05A0F0'];
+
+          this.lineChartData = {
+            labels: this.labels.reverse(),
+            datasets: [{
+              data: this.header.reverse(), 
+              label: this.op.name,
+              pointBackgroundColor: 'rgba(148,159,177,1)',
+              pointHoverBackgroundColor: '#fff',
+              pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+              fill: 'origin',
+            }]
+          }
+          
+          console.log(  objeto , 'c-map' )  
+
+        })    
+
   }
 
-  public lineChartData: ChartConfiguration['data'] = {
 
-    datasets: [
-
-      {
-        data: [ 65, 59, 80, 81, 56, 55, 40 ],
-        label: 'Dolar',
-        backgroundColor: 'rgba(148,159,177,0.2)',
-        borderColor: 'rgba(148,159,177,1)',
-        pointBackgroundColor: 'rgba(148,159,177,1)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
-        fill: 'origin',
-      }
-      
-    ],
-    labels: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July' ]
-
-  };
 
   public lineChartOptions: ChartConfiguration['options'] = {
     
@@ -66,14 +104,42 @@ export class GraficoComponent implements OnInit {
       }
     },
 
-
   };
 
-  public lineChartType: ChartType = 'line';
 
 
 
 
+
+
+
+  getDate( option: number ){
+  
+    moment.locale('es');
+    let today = moment();
+    let res: moment.Moment; 
+
+    if(option === 1){
+      // Devuelve el día, contado desde el día anterior.
+      // res = today.subtract(30, 'days').format('YYYY-MM-DD');
+      let date = today.subtract(10, 'days');
+      this.op.year = date.format('YYYY');
+      this.op.month = date.format('MM');
+      this.op.day = date.format('DD');
+
+    }else if(option === 2){
+      // Devuelve el año actual
+      // date = today.subtract(12, 'months').format('YYYY-MM');
+      this.op.year = today.format('YYYY')
+    }else {
+      // Los ultimos 12 meses
+      let date = today.subtract(12, 'months');
+      this.op.year = date.format('YYYY');
+      this.op.month = date.format('MM');
+      console.log(date, 'date-month')
+    }
+
+  }  
   
 
 }
--- src/app/indicadores/services/indicador.service.ts
+++ src/app/indicadores/services/indicador.service.ts
@@ -24,10 +24,14 @@ export class IndicadorService {
       // ultimos 30 días (añadir un día más)
       // url = `${ this.apiUrl }/dolar/posteriores/2022/08/dias/10?apikey=${ this.ApiKey }&formato=json`;
       url = `${ this.apiUrl }/${ op.name }/posteriores/${ op.year }/${ op.month}/dias/${ op.day}?apikey=${ this.ApiKey }&formato=json`;
-      console.log(url, 'url DOLAr')
-    }else{
+      console.log(url, 'url Dolar')
+    }else if( op.category === 2){
       // Año actual
-      // url = `${ this.apiUrl }/ipc/2022?apikey=${ this.ApiKey }&formato=json`; 
+      // https://api.cmfchile.cl/api-sbifv3/recursos_api/utm/posteriores/2021/09?
+      url = `${ this.apiUrl }/${ op.name }/posteriores/${ op.year }/${ op.month }?apikey=${ this.ApiKey }&formato=json`; 
+      console.log(url, 'url UTM')
+    }else{
+      // 
       url = `${ this.apiUrl }/${ op.name }/${ op.year }?apikey=${ this.ApiKey }&formato=json`; 
       console.log(url, 'url UTM')
 
@@ -37,6 +41,7 @@ export class IndicadorService {
     return this.http.get<Indicador>( url );
 
   }
+
 
   getData( op: option ){
     
@@ -55,42 +60,6 @@ export class IndicadorService {
   }
 
 
-  getIndicadorUf( id: string ): Observable<Uf>{
-
-    // ultimos 30 días (añadir un día más)
-    const url = `${ this.apiUrl }/uf/posteriores/2010/01/dias/01?apikey=${ this.ApiKey }&formato=json`;
-    
-    // Año actual
-    const urlv2 = `${ this.apiUrl }/ipc/2022?apikey=${ this.ApiKey }&formato=json`; 
-    
-    return this.http.get<Uf>( url );
-
-  }    
-
-  getGraficoDetalle( id: string): Observable<Dolar>{
-
-    // Ultimos 10 días uf (agregar un mes más)
-    const url = `${ this.apiUrl }/dolar/posteriores/2022/09/dias/2?apikey=${ this.ApiKey }&formato=json`; 
-
-    //Ultimos 12 mese utm (Agregar un mes más)
-    const urlv2 = `${ this.apiUrl }/utm/posteriores/2022/01?apikey=${ this.ApiKey }&formato=json`; 
-
-
-    return this.http.get<Dolar>( urlv2 );
-
-  }
-
-  getUnidadActual( id: string ): Observable<Dolar>{
-
-    // Resultado en espcifico
-    const url = `${ this.apiUrl }/dolar/2010/01/dias/05?apikey=${ this.ApiKey }&formato=json`; 
-
-    // Resultado en especifico
-    const urlv2 = `${ this.apiUrl }/utm/2010/01?apikey=${ this.ApiKey }&formato=json`; 
-
-    return this.http.get<Dolar>( url )
-
-  }
 
 
 
